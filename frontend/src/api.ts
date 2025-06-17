44c5bbd1057832ec00c40200a329c33e92cee997
HannaTrinh <trinhmaianhacg@gmail.com>
2025-06-17 13:30:25 -0400

    gitignore

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export async function getAll(collection: string) {
     const res = await axios.get(`${API_URL}/${collection}`);
